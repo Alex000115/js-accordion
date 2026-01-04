4ede6f2cc571f5d6ee73ca37dc308b7afc400ae9
Alex <160778418+Alex000115@users.noreply.github.com>
2026-01-04 14:41:45 +0600

    add accordion logic

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggle(i){
+  const panels = document.querySelectorAll('.panel');
+  panels.forEach((p,index)=>{
+    p.style.display = index === i && p.style.display !== 'block'
+      ? 'block' : 'none';
+  });
+}
